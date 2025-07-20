6529d1e1dfca5552a62eed4539e4514b20a9180d
anh-it2 <vuminhanhnamtruc2004@gmail.com>
2025-07-20 17:18:31 +0700

    update

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -7,12 +7,12 @@ const Card = ({image, description}) => {
     const showDescription = () => setShow(!show)
 
   return (
-        <div className='Card'>
-            {show ? <img src={image} />
+    <>
+             {show ? <img src={image} />
             : <p>{description}</p>
             }
             <button onClick={showDescription} className='infor'>Infor</button>
-        </div>
+    </>
   )
 }
 
